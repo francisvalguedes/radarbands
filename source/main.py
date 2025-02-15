5c1c2157db9c9aab72beef072844fe2cc374b33e
francisvalguedes <francisval.guedes.soares.037@ufrn.edu.br>
2025-02-14 23:11:49 -0300

    up

--- source/main.py
+++ source/main.py
@@ -4,30 +4,10 @@ Date: 2021
 """
 
 import streamlit as st
-from datetime import datetime
 import pandas as pd
 
-# from lib.pgFunctions import*
+from lib.pgFunctions import*
 
-def sensor_registration():
-    # adicionar novo ponto de referência (sensor)
-    lc_expander = st.sidebar.expander("Adicionar novo ponto de referência no WGS84", expanded=False)
-    lc_name = lc_expander.text_input('Nome', "minha localização")
-    latitude = lc_expander.number_input('Latitude', -90.0, 90.0, 0.0, format="%.6f")
-    longitude = lc_expander.number_input('Longitude', -180.0, 180.0, 0.0, format="%.6f")
-    height = lc_expander.number_input('Altura (m)', -1000.0, 2000.0, 0.0, format="%.6f")
-    #color = lc_expander.text_input('Cor', "red")
-    if lc_expander.button("Registrar nova localização"):
-        lc_add = {'name': [lc_name], 'lat': [latitude], 'lon': [longitude], 'height': [height]} # , 'color': [color]
-        if lc_name not in st.session_state.lc_df['name'].to_list():
-            if re.match('^[A-Za-z0-9_-]*$', lc_add['name'][0]):
-                st.session_state.lc_df = pd.concat([st.session_state.lc_df, pd.DataFrame(lc_add)], axis=0)
-                st.session_state.lc_df.to_csv('data/confLocalWGS84.csv', index=False)
-                lc_expander.write('Localização registrada')
-            else:
-                lc_expander.write('Escreva um nome sem caracteres especiais')
-        else:
-            lc_expander.write('Localização já existe')
 
 def main():
     """main function that provides the simplified interface for configuration,
